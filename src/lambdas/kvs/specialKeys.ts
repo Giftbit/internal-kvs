3023cb7a8ac4c34393e86cbf326539c93e5f9e17
jeff <jeff@giftbit.com>
2017-10-27 15:45:18 -0700

    Update dependencies.

--- src/lambdas/kvs/specialKeys.ts
+++ src/lambdas/kvs/specialKeys.ts
@@ -4,16 +4,16 @@ export const specialKeys: {[key: string]: SpecialKey} = {
     jwtSecret: {
         encrypted: true,
         readScopes: ["lightrailV1:token:read"],
-        writeScopes: ["lightrailV1:token"]
+        writeScopes: ["lightrailV1:token:write"]
     },
     stripeApiKey: {
         encrypted: true,
         readScopes: ["lightrailV1:stripeConnect:read"],
-        writeScopes: ["lightrailV1:stripeConnect"]
+        writeScopes: ["lightrailV1:stripeConnect:write"]
     },
     stripeOauth: {
         encrypted: true,
         readScopes: ["lightrailV1:stripeConnect:read"],
-        writeScopes: ["lightrailV1:stripeConnect"]
+        writeScopes: ["lightrailV1:stripeConnect:write"]
     }
 };
