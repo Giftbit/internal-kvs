3960e9d416df34ad475bcccf70c34a2debd436ba
Tim <tim@giftbit.com>
2017-11-21 14:47:56 -0800

    Update specialKeys.ts
    - renamed stripe connect key to `stripeAuth`.

--- src/lambdas/kvs/specialKeys.ts
+++ src/lambdas/kvs/specialKeys.ts
@@ -6,7 +6,7 @@ export const specialKeys: {[key: string]: SpecialKey} = {
         readScopes: ["lightrailV1:sharedSecret:read"],
         writeScopes: ["lightrailV1:sharedSecret:write"]
     },
-    stripeApiKey: {
+    stripeAuth: {
         encrypted: true,
         readScopes: ["lightrailV1:stripeConnect:read"],
         writeScopes: ["lightrailV1:stripeConnect:write"]
