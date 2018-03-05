f56b46171486d4f4e7049d39bc26c512012ae3b9
jeff <jeff@giftbit.com>
2018-03-05 09:32:31 -0800

    Special keys for reactionsSecureObject.

--- src/lambdas/kvs/specialKeys.ts
+++ src/lambdas/kvs/specialKeys.ts
@@ -7,11 +7,17 @@ export const specialKeys: {[key: string]: SpecialKey} = {
         readScopes: ["lightrailV1:sharedSecret:read"],
         writeScopes: ["lightrailV1:sharedSecret:write"]
     },
-    stripeAuth: {
+    reactionsSecureObject: {
         encrypted: true,
         hidden: true,
         readScopes: ["lightrailV1:stripeConnect:read"],
         writeScopes: ["lightrailV1:stripeConnect:write"]
+    },
+    stripeAuth: {
+        encrypted: true,
+        hidden: true,
+        readScopes: ["lightrailV1:react:secure:read"],
+        writeScopes: ["lightrailV1:react:secure:write"]
     },
     turnkeyPublicConfig: {
         encrypted: false,
