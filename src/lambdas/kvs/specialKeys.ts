03f087e7d8724043bfd445ac88031c3c6ab46cf6
jeff <jeff@giftbit.com>
2018-03-05 09:45:44 -0800

    Got my scopes confused.

--- src/lambdas/kvs/specialKeys.ts
+++ src/lambdas/kvs/specialKeys.ts
@@ -10,14 +10,14 @@ export const specialKeys: {[key: string]: SpecialKey} = {
     reactionsSecureObject: {
         encrypted: true,
         hidden: true,
-        readScopes: ["lightrailV1:stripeConnect:read"],
-        writeScopes: ["lightrailV1:stripeConnect:write"]
+        readScopes: ["lightrailV1:react:secure:read"],
+        writeScopes: ["lightrailV1:react:secure:write"]
     },
     stripeAuth: {
         encrypted: true,
         hidden: true,
-        readScopes: ["lightrailV1:react:secure:read"],
-        writeScopes: ["lightrailV1:react:secure:write"]
+        readScopes: ["lightrailV1:stripeConnect:read"],
+        writeScopes: ["lightrailV1:stripeConnect:write"]
     },
     turnkeyPublicConfig: {
         encrypted: false,
