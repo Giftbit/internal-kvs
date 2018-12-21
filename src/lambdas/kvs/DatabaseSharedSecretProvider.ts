7ed01577efba663480fc0baa616dc8820480add5
jeff <jeff@giftbit.com>
2018-12-20 16:09:52 -0800

    Make clearer that getSharedSecret does not and should not verify the signature of the token payload.

--- src/lambdas/kvs/DatabaseSharedSecretProvider.ts
+++ src/lambdas/kvs/DatabaseSharedSecretProvider.ts
@@ -5,18 +5,20 @@ import * as encryption from "./encryption";
 
 export class DatabaseSharedSecretProvider implements giftbitRoutes.jwtauth.sharedSecret.SharedSecretProvider {
     async getSharedSecret(token: string): Promise<string> {
-        const authPayload = (jwt.decode(token) as any);
-        if (!authPayload) {
+        // The signature of the token does not get verified because it's signed with the secret
+        // that we're fetching.
+        const unverifiedAuthPayload = (jwt.decode(token) as any);
+        if (!unverifiedAuthPayload) {
             throw new Error("Merchant signed token cannot be decoded as a JWT.");
         }
-        const auth = new giftbitRoutes.jwtauth.AuthorizationBadge(authPayload);
+        const unverifiedAuth = new giftbitRoutes.jwtauth.AuthorizationBadge(unverifiedAuthPayload);
 
-        let storedItem = await storedItemAccess.getStoredItem(auth.userId, "jwtSecret");
+        let storedItem = await storedItemAccess.getStoredItem(unverifiedAuth.userId, "jwtSecret");
         if (!storedItem) {
             throw new Error("Merchant does not have a shared secret set.");
         }
         if (storedItem.encrypted) {
-            storedItem = await encryption.dencryptStoredItem(auth, storedItem);
+            storedItem = await encryption.dencryptStoredItem(unverifiedAuth, storedItem);
         }
         return storedItem.value;
     }
