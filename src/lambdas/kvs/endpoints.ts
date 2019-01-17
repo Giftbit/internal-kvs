569484ea65b8f3cc17afa7eb1ab3e86d032bd1b7
tim.j <tim@giftbit.com>
2019-01-17 15:28:29 -0800

    Reogranized project so that unit tests could be written for each of the endpoints.

---
+++ src/lambdas/kvs/endpoints.ts
@@ -0,0 +1,150 @@
+import * as giftbitRoutes from "giftbit-cassava-routes";
+import * as storedItemAccess from "./storedItemAccess";
+import {specialKeys} from "./specialKeys";
+import {StoredItem} from "./StoredItem";
+import * as cassava from "cassava";
+import * as encryption from "./encryption";
+import {AWSError} from "aws-sdk";
+import log = require("loglevel");
+
+const defaultScope = "lightrailV1:portal";
+
+export function installEndpointsRest(router: cassava.Router): void {
+    router.route("/v1/storage")
+        .method("GET")
+        .handler(async evt => {
+            const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
+            auth.requireIds("userId");
+            auth.requireScopes(defaultScope);
+
+            let keys: string[];
+            try {
+                keys = (await storedItemAccess.listKeys(auth.userId))
+                    .filter(key => !(specialKeys[key] && specialKeys[key].hidden));
+            } catch (err) {
+                handleStoredItemAccessError(err)
+            }
+
+            return {
+                body: {
+                    keys
+                },
+                headers: {
+                    "Cache-Control": "no-cache, no-store, must-revalidate"
+                }
+            };
+        });
+
+    router.route("/v1/storage/{key}")
+        .method("GET")
+        .handler(async evt => {
+            const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
+            auth.requireIds("userId");
+
+            const key = evt.pathParameters.key;
+            if (specialKeys[key] && specialKeys[key].readScopes) {
+                auth.requireScopes(...specialKeys[key].readScopes);
+            } else {
+                auth.requireScopes(defaultScope);
+            }
+
+            let storedItem: StoredItem = null;
+            try {
+                storedItem = await storedItemAccess.getStoredItem(auth.userId, key);
+            } catch (err) {
+                handleStoredItemAccessError(err);
+            }
+            if (storedItem == null) {
+                throw new cassava.RestError(cassava.httpStatusCode.clientError.NOT_FOUND, "Resource not found.  The resource type was understood but nothing lives there.");
+            }
+            if (storedItem.encrypted) {
+                storedItem = await encryption.dencryptStoredItem(auth, storedItem);
+            }
+
+            return {
+                body: storedItem.value,
+                headers: {
+                    "Cache-Control": "no-cache, no-store, must-revalidate"
+                }
+            };
+        });
+
+    router.route("/v1/storage/{key}")
+        .method("PUT")
+        .handler(async evt => {
+            const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
+            auth.requireIds("userId");
+
+            const key = evt.pathParameters.key;
+            if (specialKeys[key] && specialKeys[key].writeScopes) {
+                auth.requireScopes(...specialKeys[key].writeScopes);
+            } else {
+                auth.requireScopes(defaultScope);
+            }
+
+            const value = evt.body;
+            if (JSON.stringify(value).length > 10 * 1024) {
+                throw new cassava.RestError(cassava.httpStatusCode.clientError.PAYLOAD_TOO_LARGE, "Payload too large.  The max value size is 10 KiB.");
+            }
+
+            let storedItem: StoredItem = {
+                giftbitUserId: auth.userId,
+                key,
+                value
+            };
+            if (specialKeys[key] && specialKeys[key].encrypted) {
+                storedItem = await encryption.encryptStoredItem(auth, storedItem);
+            }
+
+            try {
+                await storedItemAccess.setStoredItem(storedItem);
+            } catch (err) {
+                handleStoredItemAccessError(err);
+            }
+
+            return {
+                body: {
+                    success: true
+                }
+            };
+        });
+
+    router.route("/v1/storage/{key}")
+        .method("DELETE")
+        .handler(async evt => {
+            const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
+            auth.requireIds("userId");
+
+            const key = evt.pathParameters.key;
+            if (specialKeys[key] && specialKeys[key].writeScopes) {
+                auth.requireScopes(...specialKeys[key].writeScopes);
+            } else {
+                auth.requireScopes(defaultScope);
+            }
+
+            try {
+                await storedItemAccess.deleteItem(auth.userId, key);
+            } catch (err) {
+                throw handleStoredItemAccessError(err);
+            }
+
+            return {
+                body: {
+                    success: true
+                }
+            };
+        });
+
+    /**
+     * Checks if the error is an AWS retryable error.
+     * Dynamo timeouts are instances of retryable errors and as such KVS will return a 503.
+     */
+    function handleStoredItemAccessError(err: any): void {
+        if ((err as AWSError) && err.retryable) {
+            log.error(`AWS retryable error occurred. ${JSON.stringify(err)}`);
+            throw new cassava.RestError(503, "AWS Error occurred. " + err.message);
+        } else {
+            throw err;
+        }
+    }
+}
--- src/lambdas/kvs/index.ts
+++ src/lambdas/kvs/index.ts
@@ -1,13 +1,9 @@
 import * as cassava from "cassava";
 import * as giftbitRoutes from "giftbit-cassava-routes";
 import * as logPrefix from "loglevel-plugin-prefix";
-import * as encryption from "./encryption";
-import * as storedItemAccess from "./storedItemAccess";
-import {StoredItem} from "./StoredItem";
-import {specialKeys} from "./specialKeys";
 import {DatabaseSharedSecretProvider} from "./DatabaseSharedSecretProvider";
+import {installEndpointsRest} from "./endpoints";
 import log = require("loglevel");
-import {AWSError} from "aws-sdk";
 
 // Prefix log messages with the level.
 logPrefix.reg(log);
@@ -37,145 +33,7 @@ router.route(new giftbitRoutes.jwtauth.JwtAuthorizationRoute({
     errorLogFunction: log.error
 }));
 
-const defaultScope = "lightrailV1:portal";
-
-router.route("/v1/storage")
-    .method("GET")
-    .handler(async evt => {
-        const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
-        auth.requireIds("userId");
-        auth.requireScopes(defaultScope);
-
-        let keys: string[];
-        try {
-            keys = (await storedItemAccess.listKeys(auth.userId))
-                .filter(key => !(specialKeys[key] && specialKeys[key].hidden));
-        } catch (err) {
-            handleStoredItemAccessError(err)
-        }
-
-        return {
-            body: {
-                keys
-            },
-            headers: {
-                "Cache-Control": "no-cache, no-store, must-revalidate"
-            }
-        };
-    });
-
-router.route("/v1/storage/{key}")
-    .method("GET")
-    .handler(async evt => {
-        const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
-        auth.requireIds("userId");
-
-        const key = evt.pathParameters.key;
-        if (specialKeys[key] && specialKeys[key].readScopes) {
-            auth.requireScopes(...specialKeys[key].readScopes);
-        } else {
-            auth.requireScopes(defaultScope);
-        }
-
-        let storedItem: StoredItem = null;
-        try {
-            await storedItemAccess.getStoredItem(auth.userId, key);
-        } catch (err) {
-            handleStoredItemAccessError(err);
-        }
-        if (storedItem == null) {
-            throw new cassava.RestError(cassava.httpStatusCode.clientError.NOT_FOUND, "Resource not found.  The resource type was understood but nothing lives there.");
-        }
-        if (storedItem.encrypted) {
-            storedItem = await encryption.dencryptStoredItem(auth, storedItem);
-        }
-
-        return {
-            body: storedItem.value,
-            headers: {
-                "Cache-Control": "no-cache, no-store, must-revalidate"
-            }
-        };
-    });
-
-router.route("/v1/storage/{key}")
-    .method("PUT")
-    .handler(async evt => {
-        const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
-        auth.requireIds("userId");
-
-        const key = evt.pathParameters.key;
-        if (specialKeys[key] && specialKeys[key].writeScopes) {
-            auth.requireScopes(...specialKeys[key].writeScopes);
-        } else {
-            auth.requireScopes(defaultScope);
-        }
-
-        const value = evt.body;
-        if (JSON.stringify(value).length > 10 * 1024) {
-            throw new cassava.RestError(cassava.httpStatusCode.clientError.PAYLOAD_TOO_LARGE, "Payload too large.  The max value size is 10 KiB.");
-        }
-
-        let storedItem: StoredItem = {
-            giftbitUserId: auth.userId,
-            key,
-            value
-        };
-        if (specialKeys[key] && specialKeys[key].encrypted) {
-            storedItem = await encryption.encryptStoredItem(auth, storedItem);
-        }
-
-        try {
-            await storedItemAccess.setStoredItem(storedItem);
-        } catch (err) {
-            handleStoredItemAccessError(err);
-        }
-
-        return {
-            body: {
-                success: true
-            }
-        };
-    });
-
-router.route("/v1/storage/{key}")
-    .method("DELETE")
-    .handler(async evt => {
-        const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
-        auth.requireIds("userId");
-
-        const key = evt.pathParameters.key;
-        if (specialKeys[key] && specialKeys[key].writeScopes) {
-            auth.requireScopes(...specialKeys[key].writeScopes);
-        } else {
-            auth.requireScopes(defaultScope);
-        }
-
-        try {
-            await storedItemAccess.deleteItem(auth.userId, key);
-        } catch (err) {
-            throw handleStoredItemAccessError(err);
-        }
-
-        return {
-            body: {
-                success: true
-            }
-        };
-    });
-
-/**
- * Checks if the error is an AWS retryable error.
- * Dynamo timeouts are instances of retryable errors and as such KVS will return a 503.
- */
-function handleStoredItemAccessError(err: any): void {
-    if (err instanceof AWSError && err.retryable) {
-        log.error(`AWS retryable error occurred. ${JSON.stringify(err)}`);
-        throw new cassava.RestError(503, "AWS Error occurred. " + err.message);
-    } else {
-        throw err;
-    }
-}
+installEndpointsRest(router);
 
 //noinspection JSUnusedGlobalSymbols
 // Export the lambda handler with Sentry error logging supported.
